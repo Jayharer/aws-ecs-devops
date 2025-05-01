f66aba2b821001eba59289f0d7e08309952a888c
jay415404 <jay416505@gmail.com>
2025-05-02 00:12:08 +0530

    fix: ecs_repo_name output value

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,7 +2,7 @@
 output "infrastructure_output" {
   value = {
     aws_account_id  = data.aws_caller_identity.current.account_id
-    ecr_repo_name = aws_ecr_repository.dev_ecr_repo.name
+    ecs_repo_name = aws_ecr_repository.dev_ecr_repo.name
     vpc_id = aws_vpc.dev_vpc.id
   }
 }
